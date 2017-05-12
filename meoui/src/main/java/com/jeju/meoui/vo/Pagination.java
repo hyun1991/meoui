e8e64dc5d6c2ef954ddc17992cf7e1c5b8d4a950
임제현 <Administrator@Admin-PC>
2017-05-12 16:01:48 +0900

    Pagination, PagingUtil 추가

--- meoui/src/main/java/com/jeju/meoui/vo/Pagination.java
+++ meoui/src/main/java/com/jeju/meoui/vo/Pagination.java
@@ -1,15 +1,14 @@
 package com.jeju.meoui.vo;
 
-import lombok.Data;
+import lombok.*;
 
 @Data
 public class Pagination {
-	private int pageNo;
-	private int startArticle;
-	private int endArticle;
-	private int startPage;
-	private int endPage;
-	private int prev;
-	private int next;
+	private int pageNo;				//	사용자가 요청한 페이징번호
+	private int startPaging;		//	하단영역에 보여져야 하는 첫번째 페이징 번호
+	private int lastPaging;			//	하단영역에 보여져야 하는 마지막 페이징 번호
+	private int startRow;			//	뷰되는 페이지의 첫번째 게시글 번호
+	private int lastRow;			//	뷰되는 페이지의 마지막 게시글 번호
+	private int prev;				//	이전페이징
+	private int next;				//	다음페이징
 }
-
