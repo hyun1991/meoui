41f523f30040abd2bc8771acf43159ee743455e0
꾹 <@DESKTOP-3MODIDK>
2017-05-15 13:52:24 +0900

    자유게시판 vo 추가 자유게시판 댓글 vo 추가 자유게시판 mapper 추가

---
+++ src/main/java/com/jeju/meoui/vo/FreeBoard.java
@@ -0,0 +1,15 @@
+package com.jeju.meoui.vo;
+
+import java.util.Date;
+
+import lombok.*;
+
+@Data
+public class FreeBoard {
+	private int freeboardNo;			// 자유게시판 번호
+	private String freeboardTitle;		// 자유게시판 제목
+	private String freeboardContent;	// 자유게시판 내용
+	private Date freeboardDate;			// 자유게시판 작성일
+	private int freeboardCnt;			// 자유게시판 조회수
+	private String freeboardImg;			// 자유게시판 업로드 파일명
+}
