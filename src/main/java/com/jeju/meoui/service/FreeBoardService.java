470c1bfe852dbf3a2fd0ab507f5dabec64fd19d9
꾹 <@DESKTOP-3MODIDK>
2017-05-15 16:40:32 +0900

    자유게시판 dao 수정 자유게시판 서비스 추가

--- src/main/java/com/jeju/meoui/service/FreeBoardService.java
+++ src/main/java/com/jeju/meoui/service/FreeBoardService.java
@@ -1,5 +1,43 @@
 package com.jeju.meoui.service;
 
-public class FreeBoardService {
+import java.util.*;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import com.jeju.meoui.dao.*;
+import com.jeju.meoui.util.*;
+import com.jeju.meoui.vo.*;
+
+@Service
+public class FreeBoardService {
+	@Autowired
+	private FreeBoardDAO dao;
+	//1. 자유게시판  추가하기
+	public void createFreeboard(FreeBoard freeBoard) {
+		dao.insertFreeboard(freeBoard);
+	}
+	//2. 자유게시판  수정하기
+	public void modifyFreeboard(FreeBoard freeBoard) {
+		dao.updateFreeboard(freeBoard);
+	}
+	//3. 자유게시판 삭제하기
+	public void removeFreeboard(int freeboardNo) {
+		dao.deleteFreeboard(freeboardNo);
+	}
+	//4. 자유게시판 페이지별 조회하기
+	public HashMap<String, Object> getByFreeboard(int pageNo) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		int cntRow = dao.findByMax();
+		Pagination p = PagingUtil.getPagination(pageNo, cntRow);
+		List<FreeBoard> list = dao.selectAllFreeboard(p.getStartPaging(), p.getLastPaging());
+		map.put("pagination", p);
+		map.put("list", list);
+		return map;
+	}
+	//5. 자유게시판 번호별 조회
+	public FreeBoard findByFreeboard(int freeboardNo) {
+		dao.incrementCnt(freeboardNo);
+		return dao.selectFreeboard(freeboardNo);
+	}
 }
