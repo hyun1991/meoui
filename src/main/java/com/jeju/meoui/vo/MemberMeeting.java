b140a85d479926ec1e31127a6b811e65d3b4cd23
icia <icia@icia-PC>
2017-05-15 14:57:04 +0900

    모임 vo생성

---
+++ src/main/java/com/jeju/meoui/vo/MemberMeeting.java
@@ -0,0 +1,11 @@
+package com.jeju.meoui.vo;
+
+import lombok.*;
+
+@Data
+public class MemberMeeting {
+	
+	private int meetingNo;				//모임번호
+	private String meetingName;			//모임이름
+	private int meetingTotalNumber;		//모임인원수
+}
