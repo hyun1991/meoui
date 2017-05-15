3614ff38db102058c926583e092cd258d4492c04
꾹 <@DESKTOP-3MODIDK>
2017-05-15 17:21:52 +0900

    자유게시판 controller추가

--- src/main/java/com/jeju/meoui/controller/FreeBoardController.java
+++ src/main/java/com/jeju/meoui/controller/FreeBoardController.java
@@ -1,5 +1,18 @@
 package com.jeju.meoui.controller;
 
-public class FreeBoardController {
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import com.jeju.meoui.service.*;
 
+@Controller
+public class FreeBoardController {
+	@Autowired
+	private FreeBoardService service;
+	private static final Logger logger = LoggerFactory.getLogger(FreeBoardController.class);
+	//1 .자유게시판 추가하기
+	//2. 자유게시판 수정하기
+	//3. 자유게시판 삭제하기
+	//4. 자유게시판 페이지별 조회하기
+	//5. 자유게시판 번호별 조회
 }
