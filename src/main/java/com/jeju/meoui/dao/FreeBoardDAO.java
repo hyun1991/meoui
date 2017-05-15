3d893cd47405cd2cfeec490828c636889723e73a
꾹 <@DESKTOP-3MODIDK>
2017-05-15 14:49:12 +0900

    2

--- src/main/java/com/jeju/meoui/dao/FreeBoardDAO.java
+++ src/main/java/com/jeju/meoui/dao/FreeBoardDAO.java
@@ -15,13 +15,13 @@ public class FreeBoardDAO {
 	
 	// 1. 자유게시판 작성
 	public int insertFreeboard(FreeBoard freeboard) {
-		return template.insert("FreeboardMapper.insertFreeboard", freeboard);
+		return template.insert("FreeBoardMapper.insertFreeboard", freeboard);
 	}
 	// 2. 자유게시판 상세조회
 	
 	// 3. 자유게시판 리스트 조회
 	public List<FreeBoard> selectAllFreeboard(int start, int end) {
-		return null;
+		return template.selectList("FreeBoardMapper.selectAllFreeboard", start);
 	}
 	// 4. 자유게시판 수정
 	public void updateFreeboard(FreeBoard freeBoard) {
