d6757859756c4931ba6beaa2cc3c6c2c89d990c2
꾹 <@DESKTOP-3MODIDK>
2017-05-15 14:46:06 +0900

    이름통일

--- src/main/java/com/jeju/meoui/vo/FreeBoardComment.java
+++ src/main/java/com/jeju/meoui/vo/FreeBoardComment.java
@@ -5,7 +5,7 @@ import java.util.*;
 import lombok.*;
 
 @Data
-public class FreeboardComment {
+public class FreeBoardComment {
 	private int freeboardCommentNo;			// 자유게시판 댓글 번호
 	private String freeboardCommentContent;	// 자유게시판 댓글 내용
 	private Date freeboardCommentDate;		// 자유게시판 댓글 작성일
