e7eb28007da97bcc5cb766dfa344dc7d6d3bed3e
Administrator <Administrator@Admin-PC>
2017-05-16 10:57:11 +0900

    공지 사항 디에오 테스트중 마이 바티스 컴피그 수정

--- src/main/java/com/jeju/meoui/dao/NoticeDAO.java
+++ src/main/java/com/jeju/meoui/dao/NoticeDAO.java
@@ -15,35 +15,35 @@ public class NoticeDAO {
 	
 	//1. 공지게시판 작성
 	public void insertNotice(Notice notice){
-		template.insert("insertNotice", notice);
+		template.insert("NoticeMapper.insertNotice", notice);
 	}
 	//2.공지게시판 상세조회
 	public int selectNoticeList(int noticeNo){
-		return template.selectOne("selectNoticeList", noticeNo);
+		return template.selectOne("NoticeMapper.selectNoticeList", noticeNo);
 	}
 	//3.공지게시판 리스트 조회
 	public List<Notice> selectNoticeList(int startRow, int lastRow){
 		HashMap<String, Object> map = new HashMap<String, Object>();
 		map.put("startRow", startRow);
 		map.put("lastRow", lastRow);
-		return template.selectList("selectNoticeList",map);
+		return template.selectList("NoticeMapper.selectNoticeList",map);
 	}
 	//4. 공지게시글 갱신 (글제목 과 글내용 수정 가능)
    public void updateNotice(Notice notice){
-	   template.update("updateNotice", notice);
+	   template.update("NoticeMapper.updateNotice", notice);
    }
    //5. 공지게시글 삭제
    public void deleteNotice(int noticeNo){
-	   template.delete("deleteNotice", noticeNo);
+	   template.delete("NoticeMapper.deleteNotice", noticeNo);
    }
    //6.공지게시글 전체 개수 조회
    public int findByMax(){
-	  return template.selectOne("findByMax");
+	  return template.selectOne("NoticeMapper.findByMax");
    }
    
    //7. 공지게시글 조회수 증가
    public void incrementCnt(int noticeNo){
-	   template.update("incrementCnt", noticeNo);
+	   template.update("NoticeMapper.incrementCnt", noticeNo);
    }
    
 }
