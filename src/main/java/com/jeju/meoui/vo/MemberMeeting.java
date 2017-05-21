cd722fa9e2499312dae807805ea3a4592e48fc3f
Administrator <Administrator@Admin-PC>
2017-05-22 08:56:03 +0900

    모임 컬럼변경

--- src/main/java/com/jeju/meoui/vo/MemberMeeting.java
+++ src/main/java/com/jeju/meoui/vo/MemberMeeting.java
@@ -9,6 +9,7 @@ public class MemberMeeting {
 	
 	private int meetingNo;				//모임번호
 	private String meetingName;			//모임이름
+	private String meetingImg;			//모임대표이미지
 	private int meetingTotalNumber;		//모임인원수
 
 
