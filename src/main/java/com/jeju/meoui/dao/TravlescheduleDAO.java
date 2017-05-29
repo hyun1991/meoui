b7310cd33530a6265dd3e1bc92fe1a6272a48214
Administrator <Administrator@Admin-PC>
2017-05-29 16:47:09 +0900

    !!!!

--- src/main/java/com/jeju/meoui/dao/TravlescheduleDAO.java
+++ src/main/java/com/jeju/meoui/dao/TravlescheduleDAO.java
@@ -1,5 +1,7 @@
 package com.jeju.meoui.dao;
 
+import java.util.*;
+
 import org.mybatis.spring.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
@@ -14,43 +16,35 @@ public class TravlescheduleDAO {
 	public void insertTravle(Travleschedule travleschedule){
 		template.insert("TravlescheduleMapper.insertTravle", travleschedule);
 	}
-/*
- * 	<!-- 1. 일정추가하기 -->
-	<insert id="insertTravle" parameterType="Travleschedule">
-		<selectKey keyProperty="travlescheduleNo" resultType="int" order="BEFORE">
-	    	select travleschedule_seq.nextval from dual
-		</selectKey>
-		<![CDATA[
-			insert into travleschedule(travleschedule_no, member_no, travleschedule_date, schedule)
-			values(#{travlescheduleNo}, #{memberNo}, #{travlescheduleDate}, #{schedule})
-		]]>
-	</insert>
-	<!-- 2. 일정수정하기 -->
-	<update id="updateTravle" parameterType="Travleschedule">
-		<![CDATA[
-			update travleschedule set schedule=#{schedule} where travleschedule_no=#{travlescheduleNo} and
-			member_no=#{memberNo} and travleschedule_date=#{travlescheduleDate}
-		]]>
-	</update>
+	//	일정수정하기
+	public void updateTravle(Travleschedule travleschedule){
+		template.update("TravlescheduleMapper.updateTravle", travleschedule);
+	}
+	//	일정번호별 고객번호 조회하기
+	public int selectByMemberNo(int travlescheduleNo){
+		return template.selectOne("TravlescheduleMapper.selectByMemberNo", travlescheduleNo);
+	}
+	//	일정삭제하기
+	public void deleteTravle(int travlescheduleNo, int memberNo){
+		HashMap<String, Object>map= new HashMap<String, Object>();
+		map.put("travlescheduleNo", travlescheduleNo);
+		map.put("memberNo", memberNo);
+		template.selectOne("TravlescheduleMapper.deleteTravle", map);
+	}
+	//	일정번호 및 고객번호별 일정정보 조회하기
+	public Travleschedule findByTravle(int travlescheduleNo, int memberNo){
+		HashMap<String, Object>map= new HashMap<String, Object>();
+		map.put("travlescheduleNo", travlescheduleNo);
+		map.put("memberNo", memberNo);
+		return template.selectOne("TravlescheduleMapper.findByTravle", map);
+	}
+	//	고객번호별 일정전체리스트 조회하기
+	public List<Travleschedule> findAllMemberNo(int memberNo){
+		return template.selectList("TravlescheduleMapper.findAllMemberNo", memberNo);
+	}
+	//	최근입력된 일정번호 조회하기
+	public int getByTravleNo(){
+		return template.selectOne("TravlescheduleMapper.getByTravleNo");
+	}
 
-	<!-- 4. 일정번호별 고객번호 조회하기 -->
-	<select id="selectByMemberNo" parameterType="int" resultType="int">
-		select member_no from travleschedule where travleschedule_no=#{travlescheduleNo}
-	</select>
-
-	<!-- 5. 일정삭제하기 -->
-	<delete id="deleteTravle" parameterType="HashMap">
-		delete from travleschedule where travleschedule_no=#{travlescheduleNo} and member_no=#{memberNo}
-	</delete>
-
-	<!-- 6. 일정번호 및 고객번호별 일정정보 가져오기 -->
-	<select id="findByTravle" parameterType="HashMap" resultType="Travleschedule">
-		select * from travleschedule where travleschedule_no=#{travlescheduleNo} and member_no=#{memberNo}
-	</select>
-	<!-- 7. 고객번호별 일정전체리스트 출력하기 -->
-	<select id="findAllMemberNo" parameterType="int" resultType="Travleschedule">
-		select * from travleschedule where member_no=#{memberNo}
-	</select>
-
- * */
 }
