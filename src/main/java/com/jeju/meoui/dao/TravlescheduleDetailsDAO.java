1e6e747d53c06e543086be01c4b2fa55851b9f81
Administrator <Administrator@Admin-PC>
2017-05-29 16:15:48 +0900

    !!!!

---
+++ src/main/java/com/jeju/meoui/dao/TravlescheduleDetailsDAO.java
@@ -0,0 +1,15 @@
+package com.jeju.meoui.dao;
+
+public class TravlescheduleDetailsDAO {
+/*
+ * 	<!-- 1. 일정상세 추가하기 -->
+	<insert id="insertTravleDetails" parameterType="TravlescheduleDetails">
+		insert into travleschedule_details(site_no, travleschedule_no, member_no)
+		values(#{siteNo}, #{travlescheduleNo}, #{memberNo})
+	</insert>
+	<!-- 2. 일정상세 삭제하기 -->
+	<delete id="deleteTravleDetails" parameterType="HashMap">
+		delete from travleschedule_details where travleschedule_no=#{travlescheduleNo} and member_no=#{memberNo}
+	</delete>
+ * */
+}
