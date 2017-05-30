f40710dd71b81f5a2b8b88ca3eca3fa870344374
Administrator <Administrator@Admin-PC>
2017-05-30 13:45:23 +0900

    풀안되서

--- src/main/java/com/jeju/meoui/vo/FreeBoardComment.java
+++ src/main/java/com/jeju/meoui/vo/FreeBoardComment.java
@@ -5,8 +5,12 @@ import java.util.*;
 import lombok.*;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FreeBoardComment {
 	private int freeboardCommentNo;			// 자유게시판 댓글 번호
 	private String freeboardCommentContent;	// 자유게시판 댓글 내용
-	private Date freeboardCommentDate;		// 자유게시판 댓글 작성일
+	private Date freeboardCommentDate;// 자유게시판 댓글 작성일
+	private int freeboardNo;
+	private int memberNo;
 }
